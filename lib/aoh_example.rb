610bb25b33e706268e6e2e03c1a406ce6c2e9d21
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-18 03:14:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,8 @@ def assembled_aoh
 end
 
 def literal_aoh
-  aoh = Array.new.({})
+  aoh = Array.new
+  aoh = hash.new
   
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
